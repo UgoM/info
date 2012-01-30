c1d1b6539605ae55c4e2faf441fa6676db1c03a9
Tunaki <guillaume.boue@espci.fr>
2012-01-30 15:16:16 +0100

    test Guillaume

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include <QApplication>
+﻿#include <QApplication>
 #include <QPushButton>
  
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
  
-    QPushButton bouton("Salut les Zéros, la forme ?");
+    QPushButton bouton("Salut les Zéros, la forme ? Test");
     bouton.setText("Pimp mon encore !");
  
     bouton.show();
