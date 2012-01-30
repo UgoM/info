25d465e190561137c65d9680987c90946ae28a7d
Marine <favier.marine@gmail.com>
2012-01-30 15:07:08 +0100

    encore un test, Marine

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
  
     QPushButton bouton("Salut les Zéros, la forme ?");
-    bouton.setText("Pimp mon toto !");
+    bouton.setText("Pimp mon encore !");
  
     bouton.show();
  
