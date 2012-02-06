60345b075756032c835bf60d414d139998497b11
Ugo M <marchand.ugo@orange.fr>
2012-02-06 15:29:17 +0100

    oubli commit precedent, ajout des .cpp

---
+++ Brain.cpp
@@ -0,0 +1,18 @@
+#include "Brain.h"
+
+Brain::Brain()
+{
+}
+
+
+void Brain::sendTo(int idClient, QString message)
+{
+}
+
+void Brain::sendToAll(QString message)
+{
+}
+
+void Brain::processReceive(QString message)
+{
+}
---
+++ Game.cpp
@@ -0,0 +1,25 @@
+#include "Game.h"
+
+Game::Game()
+{
+}
+
+void Game::sendTo(int idClient, QString message)
+{
+}
+
+void Game::sendToAll(QString message)
+{
+}
+
+void Game::processReceive(QString message)
+{
+}
+
+void Game::processClick()
+{
+}
+
+void Game::processKey()
+{
+}
