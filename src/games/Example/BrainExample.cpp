83be2a9b9af3752747db343ce1687e975d242fee
Ugo M <marchand.ugo@orange.fr>
2012-06-01 12:42:35 +0200

    fixed error in doc of BrainExample and GameExample

--- src/games/Example/BrainExample.cpp
+++ src/games/Example/BrainExample.cpp
@@ -8,11 +8,12 @@ void BrainExample::init()
     // name = "Awesome Game";
 }
 
-/** \brief What to do when game receives data from server
+/** \brief What to do when game receives data from clients
   * \param dat : data received
   *
   * Here are only sent data. It is up to you to develop how your will encode
-  * and decode your datas. Use send(QByteArray dat) to send back data to Brain.
+  * and decode your datas. Use sendToAll(QByteArray dat) to send back data to 
+  * Game.
   */
 void BrainExample::processReceive(QByteArray dat)
 {
--- src/games/Example/GameExample.cpp
+++ src/games/Example/GameExample.cpp
@@ -1,12 +1,12 @@
 #include "src/games/Example/GameExample.h"
 
 
-/** \brief What to do when game receives data from clients
+/** \brief What to do when game receives data from server
   * \param dat : data received
   *
   * Here are only sent data. It is up to you to develop how your will encode
   * and decode your datas. Use sendToAll(QByteArray dat) to send back data to 
-  * Clients.
+  * Brain.
   */
 void GameExample::processReceive(QByteArray block)
 {
